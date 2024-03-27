051cdfb88c2e4a0892eb9acb339976089072ca05
ripplefcl <ripple@ripplefcl.com>
2024-03-27 15:13:24 +0000

    added grabbing upstream key changes

--- server/client.py
+++ server/client.py
@@ -144,9 +144,15 @@ class BWSClient:
 
     def get_secret_by_key(self, secret_key, org_id):
         if not self.secret_key_map or time.time() - self.secret_key_map_refresh > self.secret_info_ttl:
-            self.prom_client.tick_cache_miss("key_map")
             logger.debug("regenerating secret key map")
             self._gen_secret_key_map(org_id)
+            self.prom_client.tick_cache_miss("key_map")
+
+        elif not secret_key in self.secret_key_map:
+            logger.debug("regenerating secret key map")
+            self._gen_secret_key_map(org_id)
+            self.prom_client.tick_cache_miss("key_map")
+
         else:
             self.prom_client.tick_cache_hits("key_map")
 
