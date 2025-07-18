f1e69681e19ae6d9b7ee15d12733a7e016b10ddf
rippleFCL <github@ripple.contact>
2025-07-18 15:44:54 +0100

    feat: added sentry

--- server/server.py
+++ server/server.py
@@ -84,6 +84,16 @@ except KeyError:
     else:
         raise ValueError("a Unknown region was provided")
 
+if os.environ.get("ENABLE_SENTRY", "false").lower() == "true":
+    import sentry_sdk
+
+    sentry_sdk.init(
+        dsn="https://dc49189fab59b5237fe2d73a6baba08d@sentry.beryju.io/7",
+        # Add data like request headers and IP for users,
+        # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+        send_default_pii=True,
+    )
+
 
 api = FastAPI()
 
